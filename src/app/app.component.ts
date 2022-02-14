96250538c5fc5ea2a5fe04f1190c1ff42267dcd6
Nauman Khalid <wp061005@gmail.com>
2022-02-14 18:21:19 -0500

    Basic form developed using Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import{NgForm} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'basic_form';
+  title = 'Basic Form';
+
+  userData:any={};
+  
+
+  getData(data:NgForm){
+    console.warn(data);
+    this.userData=data;
+  }
 }
